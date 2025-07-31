9790dfccf609b0bef526e30b83543055e9b4f77a
Lolyparty <ololadeolayiwola01@gmail.com>
2025-07-31 12:46:11 +0100

    updated types

---
+++ src/types.ts
@@ -0,0 +1,13 @@
+export interface MyFormData {
+    email: string;
+    password: string;
+};
+
+export interface GiveawayCardProps {
+    title: string;
+    description: string;
+    endDate: string; 
+    organizer: string;
+    image: string; 
+    id: string; 
+}
